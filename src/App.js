cdb9fe304974bbf2aefa4709db38fad2b697b785
christinaguliuzza <christina.guliuzza@gmail.com>
2021-12-14 00:03:50 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+import NavBar from './components/navbar/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <NavBar />
+        <p>Christina Guliuzza</p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <div>
+            <p>NAV</p>
+            <a className="App-link" href="https://tinacaptures.com/category/software-engineering/">Blog</a>
+            <br/>
+            <a className="App-link" href="https://www.linkedin.com/in/christina-guliuzza-668354133/">LinkedIn</a>
+            <br/>
+            <a className="App-link" href="https://github.com/cguliuzza">Github</a>
+        </div>
+    )
+}
+
+export default NavBar
