63d66ba28180e35073a2bda888ff1f9e84c7cf8b
christinaguliuzza <christina.guliuzza@gmail.com>
2021-12-18 15:00:15 -0800

    add css

--- src/components/accordion/Accordion.js
+++ src/components/accordion/Accordion.js
@@ -12,7 +12,7 @@ const Accordion = () => {
                     </article>
                 </div>
                 <div>
-                    <input id="ac-2" name="accordion-1" type="checkbox" checked />
+                    <input id="ac-2" name="accordion-1" type="checkbox" />
                     <label for="ac-2">How we work</label>
                     <article class="ac-medium">
                         <p>Some content... </p>
