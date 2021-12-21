84a7d9eeed1ed79bdb85844b31758ca1fe08188f
christinaguliuzza <christina.guliuzza@gmail.com>
2021-12-21 14:38:36 -0800

    click event listener takes 2 clicks to open

--- src/components/accordion/Accordion.js
+++ src/components/accordion/Accordion.js
@@ -2,9 +2,30 @@ import React from 'react'
 import './Accordion.css';
 
 const Accordion = () => {
+    const onPanelClick = () => {
+        let acc = document.getElementsByClassName("accordion");
+        let i;
+    
+        for (i = 0; i < acc.length; i++) {
+            acc[i].addEventListener("click", function() {
+                this.classList.toggle("active");
+                let panel = this.nextElementSibling;
+
+                (panel.style.display === "block") ? (panel.style.display = "none") : 
+                (panel.style.display = "block")
+
+                if (panel.style.maxHeight) {
+                    panel.style.maxHeight = null;
+                } else {
+                    panel.style.maxHeight = panel.scrollHeight + "px";
+                }
+            });
+        }
+    }
+    
     return (
         <>
-            <section class="ac-container">
+            <section class="ac-container" onClick={onPanelClick}>
                 <button class="accordion">Section 1</button>
                 <div class="panel">
                 <p>Lorem ipsum...</p>
